a6d4b4add853b6d86a9b9e835afbe69bd81cdbd6
Simon Voorhees <simonvoorhees@rightpoint.com>
2022-12-21 09:23:53 -0500

    made updates to solve issues that pa11y pointed out. Fixed a bug that was causing alt text to not appear. Added aria-labels to inputs and buttons. Switched from HashRouter to BrowserRouter to make the urls no longer contain #'s

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -8,13 +8,14 @@ const Gallery = props => {
   // map variables to each item in fetched image array and return image component
   if (results.length > 0) {
     images = results.map(image => {
+      console.log(props);
       let farm = image.farm;
       let server = image.server;
       let id = image.id;
       let secret = image.secret;
       let title = image.title;
       let url = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_m.jpg`;
-      return <Image url={url} key={id} alt={title} />;
+      return <Image url={url} key={id} alt={`${title}`} />;
     });
   } else {
     noImages = <NoImages />; // return 'not found' component if no images fetched
